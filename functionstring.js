0a76f300b0435333262f7f10c24ef3c9513c65ff
marcello.albuca@gmail.com <marcello.albuca@gmail.com>
2021-05-03 22:42:17 -0300

    manipulando string

---
+++ functionstring.js
@@ -0,0 +1,14 @@
+var simples = "Marcello Cavalcanti "
+
+var email = "marcello@teste.com  "  
+
+console.log(simples.toLowerCase())
+console.log(simples.toUpperCase())
+console.log(email.trim())
+console.log(simples.replace('M', 'm'))
+
+
+
+
+
+
